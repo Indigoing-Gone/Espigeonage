af27d8a06e528ebdf80e0b4ac6a62863c7d91aab
Indigoing-Gone <155276295+Indigoing-Gone@users.noreply.github.com>
2025-12-08 16:43:43 -0500

    [ADD] Playtest assets

--- Espigeonage/Assets/Programming/Manipulation/Grab/InspectGrabber.cs
+++ Espigeonage/Assets/Programming/Manipulation/Grab/InspectGrabber.cs
@@ -6,6 +6,16 @@ public class InspectGrabber : Grabber
     [SerializeField] protected GrabData inspectData;
     public bool IsInspecting { get; private set; }
 
+    private void Start()
+    {
+        IGrabbable _grabbableChild = GetComponentInChildren<IGrabbable>(false);
+        if (_grabbableChild != null)
+        {
+            SetGrabbable(_grabbableChild);
+            Grab();
+        }
+    }
+
     public override void Grab()
     {
         IsInspecting = false;
